b0c0485d6a80518831c82f5d687bed3c1711ad2f
Teodor Coman <teodor.coman@mindit.ro>
2018-08-15 18:22:45 +0200

    ok

--- src/main/java/com/sda/forum/ForumApplication.java
+++ src/main/java/com/sda/forum/ForumApplication.java
@@ -2,6 +2,10 @@ package com.sda.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 @SpringBootApplication
 public class ForumApplication {
@@ -10,6 +14,21 @@ public class ForumApplication {
 		SpringApplication.run(ForumApplication.class, args);
 	}
 
+	@Bean
+	public SpringTemplateEngine templateEngine() {
+		SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+		templateEngine.setTemplateResolver(thymeleafTemplateResolver());
+		return templateEngine;
+	}
 
+	@Bean
+	public ClassLoaderTemplateResolver thymeleafTemplateResolver() {
+		ClassLoaderTemplateResolver templateResolver
+				= new ClassLoaderTemplateResolver();
+		templateResolver.setPrefix("templates/");
+		templateResolver.setSuffix(".html");
+		templateResolver.setTemplateMode("HTML5");
+		return templateResolver;
+	}
 
 }
