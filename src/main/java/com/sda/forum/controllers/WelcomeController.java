a46b2287d4bcddcf31722d4685180b9c71e8e220
Teodor Coman <teodor.coman@mindit.ro>
2018-08-15 21:37:41 +0200

    ok

--- src/main/java/com/sda/forum/controllers/WelcomeController.java
+++ src/main/java/com/sda/forum/controllers/WelcomeController.java
@@ -3,6 +3,7 @@ package com.sda.forum.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -11,8 +12,29 @@ public class WelcomeController {
 
 
     @GetMapping("/hello")
+    // for query param
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("message", name);
+        return "welcome.html";
+    }
+
+    @GetMapping("/hello/{name}")
+    public String greeting2(@PathVariable("name") String name, Model model) {
+        model.addAttribute("message", name);
+        return "welcome.html";
+    }
+
+    @GetMapping("/hi/{name:[\\d]+}")
+    public String greeting3(@PathVariable("name") String name, Model model) {
+        model.addAttribute("message", name);
+        return "welcome.html";
+    }
+
+
+    @GetMapping({"/hi/{name}","/whatsup/{name}"})
+    //ambiguous with previous
+    public String greeting4(@PathVariable("name") String name, Model model) {
+        model.addAttribute("message", name+"34");
         return "welcome.html";
     }
 
